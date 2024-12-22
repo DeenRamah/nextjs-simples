8ee0b967ad9cfec08102f2a6f0277d2d3cac7d33
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-12-22 06:26:36 +0300

    Create datastorage.ts

---
+++ datastorage.ts
@@ -0,0 +1,87 @@
+//data representation fot different types
+export const appartmentTypeData = [
+  {
+  id: 1,
+  icon: "/images/i",
+  type: "House",
+  number: 12,
+  },
+  {
+  id:2,
+  icon: "/images..",
+  type: "Land",
+  number: 13,
+  },
+  {
+  id: 3,
+  icon: "/images/i",
+  type: "House",
+  number: 10,
+  },
+  {
+  id:4,
+  icon: "/images..",
+  type: "Land",
+  number: 13,
+  },
+];
+//Data representing specific property listing
+
+export const properties =[
+  {
+    id:1, 
+    propertyName: "Nairobi homes",
+    location: "Oj launge",
+    bedrooms:1,
+    bathrooms: 2,
+    size:1200,
+    price: 450000,
+    imageUrl: "/images/p1.jpg",
+  },
+  {
+    id:2,
+    proopertyName: "Jalde Apartments",
+    location: "Ruiru",
+    size: 1300,
+    imageUrl: "/images/..",
+    price: 5000000,
+    
+  },
+  
+];
+
+//Data representing cities that has properties availabel
+
+export const cities =[
+  {
+  id: 1,
+  image: "/images",
+  cityName: "Nakuru",
+  numberofProperties: 120,
+  },
+  {
+    id: 2,
+    image: "/images/..",
+    cityName: "Nairobi",
+    numberofProperties: 100,
+  },
+];
+
+//data representing user reviews
+
+export const userReviewData = [
+  {
+    id: 1,
+    name: "Deen Ramah",
+    profession: "Real estate Agent",
+    userImage: "/images..",
+    review: "..."
+  },
+  {
+    id: 2,
+    name: "Taliah Deen",
+    profession: "Property Investor",
+    userImage: "/image...",
+    review: "...",
+  },
+];
